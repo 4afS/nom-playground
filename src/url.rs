b0f86a38c20ba74593dc23374fa1dbe926cf2a90
4afS <4afS.h1@gmail.com>
2021-02-20 20:47:36 +0900

    add a brief test

--- src/url.rs
+++ src/url.rs
@@ -30,3 +30,25 @@ pub struct URL {
     pub query: Vec<Query>,
     pub fragment_id: Option<FragmentId>,
 }
+
+pub fn parse_url(_: &str) -> IResult<&str, URL> {
+    panic!()
+}
+
+#[test]
+fn parse_url_test() {
+    assert_eq!(
+        parse_url("https://example.com:80/a/b?id=10#Index"),
+        Ok((
+            "",
+            URL {
+                scheme: Scheme::HTTPS,
+                host: Host("example.com".to_string()),
+                port: Some(Port(80)),
+                path: Path("/a/b".to_string()),
+                query: vec!(Query("id".to_string(), "10".to_string())),
+                fragment_id: Some(FragmentId("Index".to_string()))
+            }
+        ))
+    );
+}
